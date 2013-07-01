17a091a6da09f9d709159af18270f0ccf0be10e8
Ivan Shubin <ivan.ishubin@gmail.com>
2013-07-01 23:29:18 +0200

    Fixed test for width and height specs

--- src/main/java/net/mindengine/galen/specs/Range.java
+++ src/main/java/net/mindengine/galen/specs/Range.java
@@ -1,5 +1,8 @@
 package net.mindengine.galen.specs;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class Range {
 
     public Range(Integer from, Integer to) {
@@ -19,5 +22,30 @@ public class Range {
     }
     public static Range between(int from, int to) {
         return new Range(Math.min(from, to), Math.max(from, to));
+    }
+    
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(13, 19).append(from).append(to).toHashCode();
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Range)) {
+            return false;
+        }
+        Range rhs = (Range)obj;
+        return new EqualsBuilder().append(from, rhs.from).append(to, rhs.to).isEquals();
+    }
+    
+    @Override
+    public String toString() {
+        return "Range(" + from + ", " + to + ")";
     }
 }
