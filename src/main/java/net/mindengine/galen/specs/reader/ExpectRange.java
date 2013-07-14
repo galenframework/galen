92afa3b1e6e710847f88401630e75962b632368d
Ivan Shubin <ivan.ishubin@gmail.com>
2013-07-14 22:01:32 +0200

    Added license

--- src/main/java/net/mindengine/galen/specs/reader/ExpectRange.java
+++ src/main/java/net/mindengine/galen/specs/reader/ExpectRange.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+* Copyright 2013 Ivan Shubin http://mindengine.net
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* 
+*   http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+******************************************************************************/
 package net.mindengine.galen.specs.reader;
 
 import static net.mindengine.galen.specs.reader.Expectations.isDelimeter;
--- src/test/java/net/mindengine/galen/tests/validation/ValidationTest.java
+++ src/test/java/net/mindengine/galen/tests/validation/ValidationTest.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+* Copyright 2013 Ivan Shubin http://mindengine.net
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* 
+*   http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+******************************************************************************/
 package net.mindengine.galen.tests.validation;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,6 +28,7 @@ import net.mindengine.galen.components.validation.MockedPageElement;
 import net.mindengine.galen.page.Page;
 import net.mindengine.galen.page.PageElement;
 import net.mindengine.galen.page.Rect;
+import net.mindengine.galen.specs.Spec;
 import net.mindengine.galen.specs.SpecContains;
 import net.mindengine.galen.validation.PageValidation;
 import net.mindengine.galen.validation.ValidationError;
@@ -26,19 +42,16 @@ public class ValidationTest {
     private static final boolean CONTAINS_PARTLY = true;
 
 
-    @Test(dataProvider="provideGoodContainsSamples")
-    public void shouldPassValidation_forSpec_contains(SpecContains spec, Page page) {
+    @Test(dataProvider="provideGoodSamples")
+    public void shouldPassValidation(Spec spec, Page page) {
         PageValidation validation = new PageValidation(page, null);
         ValidationError error = validation.check("object", spec);
         
         assertThat(error, is(nullValue()));
     }
     
-    //TODO make "contains partly" and simple "contains"
-    
-    
-    @Test(dataProvider="provideBadContainsSamples")
-    public void shouldGiveError_forSpec_contains(SpecContains spec, Page page, ValidationError expectedError) {
+    @Test(dataProvider="provideBadSamples")
+    public void shouldGiveError(Spec spec, Page page, ValidationError expectedError) {
         PageValidation validation = new PageValidation(page, null);
         ValidationError error = validation.check("object", spec);
         
@@ -49,8 +62,9 @@ public class ValidationTest {
     
     @SuppressWarnings("serial")
     @DataProvider
-    public Object[][] provideGoodContainsSamples() {
+    public Object[][] provideGoodSamples() {
         return new Object[][] {
+          /* Contains */
           row(specContains(CONTAINS_FULLY, "menu", "button"), page(new HashMap<String, PageElement>(){{
               put("object", element(10, 10, 100, 100));
               put("menu", element(11, 11, 10, 10));
@@ -66,13 +80,16 @@ public class ValidationTest {
               put("menu", element(0, 0, 100, 72));
               put("button", element(5, 5, 100, 70));
           }})),
+          
+          /* */
         };
     }
     
     @SuppressWarnings("serial")
     @DataProvider
-    public Object[][] provideBadContainsSamples() {
+    public Object[][] provideBadSamples() {
         return new Object[][] {
+          /* Contains */
           row(specContains(false, "menu", "button"), page(new HashMap<String, PageElement>(){{
               put("object", element(10, 10, 100, 100));
               put("menu", element(9, 11, 10, 10));
@@ -84,6 +101,8 @@ public class ValidationTest {
               put("menu", element(50, 50, 110, 10));
               put("button", element(10, 10, 101, 40));
           }}), new ValidationError(new Rect(10, 10, 100, 100), "Objects \"menu\", \"button\" are outside the specified element")),
+          
+          /* */
         };
     }
     
