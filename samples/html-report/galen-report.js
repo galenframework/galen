f258852ba4886b3e85be5cc9d4606913c4cc4d88
Ivan Shubin <ivan.ishubin@gmail.com>
2015-02-08 15:36:12 +0100

    Added render for complete report structure and basic expand-collapse handling

--- samples/html-report/galen-report.js
+++ samples/html-report/galen-report.js
@@ -13,10 +13,58 @@ function createTemplate(templateId) {
 
 
 var _GalenReport = {};
+
+function safeHtml(html) {
+    return new Handlebars.SafeString(html);
+}
 function createGalenReport() {
     Handlebars.registerHelper("renderNode", function (node) {
         if (node !== null && node !== undefined) {
-            return new Handlebars.SafeString(_GalenReport.tpl.reportNode(node));
+            if (node.type === "node")  {
+                return safeHtml(_GalenReport.tpl.reportNode(node));
+            } else if (node.type === "text") {
+                return safeHtml(_GalenReport.tpl.reportNodeText(node));
+            } else if (node.type === "layout") {
+                return safeHtml(_GalenReport.tpl.layout(node));
+            }
+        }
+        return "";
+    });
+
+    Handlebars.registerHelper("renderLayoutSection", function (section) {
+        if (section !== null && section !== undefined) {
+            return safeHtml(_GalenReport.tpl.layoutSection(section));
+        }
+    });
+
+    Handlebars.registerHelper("renderLayoutObject", function (object) {
+        if (object !== null && object !== undefined) {
+            return safeHtml(_GalenReport.tpl.layoutObject(object));
+        }
+    });
+
+    Handlebars.registerHelper("renderLayoutCheck", function (check) {
+        if (check !== null && check !== undefined) {
+            return safeHtml(_GalenReport.tpl.layoutCheck(check));
+        }
+    });
+
+    Handlebars.registerHelper("renderSublayout", function (sublayout) {
+        if (sublayout !== null && sublayout !== undefined) {
+            return safeHtml(_GalenReport.tpl.sublayout(sublayout));
+        }
+    });
+
+    Handlebars.registerHelper("formatReportTime", function (time) {
+        if (time !== null && time !== undefined) {
+        var date = new Date(time * 1000);
+            var hh = date.getUTCHours();
+            var mm = date.getUTCMinutes();
+            var ss = date.getSeconds();
+            if (hh < 10) {hh = "0"+hh;}
+            if (mm < 10) {mm = "0"+mm;}
+            if (ss < 10) {ss = "0"+ss;}
+            return hh + ":" + mm + ":" + ss;
         }
         return "";
     });
@@ -24,11 +72,26 @@ function createGalenReport() {
     _GalenReport = {
         tpl: {
            main: createTemplate("report-tpl"),
-           reportNode: createTemplate("report-node-tpl")
+           reportNode: createTemplate("report-node-tpl"),
+           reportNodeText: createTemplate("report-node-text-tpl"),
+           layout: createTemplate("report-layout-tpl"),
+           layoutSection: createTemplate("report-layout-section-tpl"),
+           layoutObject: createTemplate("report-layout-object-tpl"),
+           layoutCheck: createTemplate("report-layout-check-tpl"),
+           sublayout: createTemplate("report-layout-sublayout-tpl")
         },
 
         render: function (id, reportData) {
             setHtml(id, this.tpl.main(reportData));
+
+            $("a.expand-link").click(function () {
+
+                var container = $(this).next(".expand-container");
+                if (container.length === 0) {
+                    container = $(this).next().next(".expand-container");
+                }
+                container.slideToggle('fast');
+            });
         }
     };
 
