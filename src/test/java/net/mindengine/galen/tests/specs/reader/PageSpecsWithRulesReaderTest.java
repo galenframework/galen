67a20d973041b14b845188b101af15e01b63740e
Ivan Shubin <ishubin@ebay.com>
2015-02-22 23:09:53 +0100

    Added more tests for rules parsing

--- src/test/java/net/mindengine/galen/tests/specs/reader/PageSpecsWithRulesReaderTest.java
+++ src/test/java/net/mindengine/galen/tests/specs/reader/PageSpecsWithRulesReaderTest.java
@@ -2,6 +2,7 @@ package net.mindengine.galen.tests.specs.reader;
 
 import net.mindengine.galen.components.specs.ExpectedSpecObject;
 import net.mindengine.galen.page.Page;
+import net.mindengine.galen.parser.SyntaxException;
 import net.mindengine.galen.specs.reader.page.PageSpec;
 import net.mindengine.galen.specs.reader.page.PageSpecReader;
 import org.testng.annotations.Test;
@@ -11,6 +12,7 @@ import java.util.Properties;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Created by ishubin on 2015/02/22.
@@ -55,6 +57,51 @@ public class PageSpecsWithRulesReaderTest {
         assertThat(ExpectedSpecObject.convertSection(pageSpec.getSections().get(0)),
                 contains(new ExpectedSpecObject("login-button")
                         .withSpecs("visible")));
+    }
+
+
+    @Test
+    public void shouldParsePageSpec_withParameterizedRule_containingObjectAndSpecs() throws IOException {
+        PageSpec pageSpec = readPageSpec("parameterized-rule-object-and-specs.spec");
+
+        assertThat(ExpectedSpecObject.convertSection(pageSpec.getSections().get(0)),
+                contains(new ExpectedSpecObject("login-button")
+                        .withSpecs("height: 100px")));
+    }
+
+    @Test
+    public void shouldParsePageSpec_withParameterizedRule_containingOnlySpecs() throws IOException {
+        PageSpec pageSpec = readPageSpec("parameterized-rule-only-specs.spec");
+
+        assertThat(ExpectedSpecObject.convertSection(pageSpec.getSections().get(0)),
+                contains(new ExpectedSpecObject("login-button")
+                        .withSpecs("height: 100px")));
+    }
+
+    @Test
+    public void shouldParsePageSpec_withParameterizedRule_containingOnlySpecs_andReusingObjectName_asParameter() throws IOException {
+        PageSpec pageSpec = readPageSpec("parameterized-rule-only-specs-with-objectname-parameter.spec");
+
+        assertThat(ExpectedSpecObject.convertSection(pageSpec.getSections().get(0)),
+                contains(new ExpectedSpecObject("login-button")
+                        .withSpecs("height: 100px")));
+    }
+
+
+    @Test
+    public void shouldThrowError_whenRuleIsNotMatched() throws IOException {
+        assertThatSyntaxExceptionIsThrownFor("error-rule-not-matched.spec",
+                "There are no rules that matching this: | Login button should ve invisible");
+    }
+
+    private void assertThatSyntaxExceptionIsThrownFor(String pageSpecPath, String expectedMessage) throws IOException {
+        try {
+            readPageSpec("parameterized-rule-only-specs-with-objectname-parameter.spec");
+            throw new RuntimeException("The expected exception was not thrown");
+        } catch (SyntaxException ex) {
+            assertThat(ex.getMessage(), is(expectedMessage));
+        }
+
     }
 
     private PageSpec readPageSpec(String specName) throws IOException {
