cdba89946c8ab70c88ef0ae571f649e085f36c1b
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-08-07 10:29:50 +0200

    Cleaning up code

--- galen-core/src/main/java/com/galenframework/utils/VersionUtil.java
+++ galen-core/src/main/java/com/galenframework/utils/VersionUtil.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2015 Ivan Shubin http://galenframework.com
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,8 @@
 package com.galenframework.utils;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.URL;
@@ -27,6 +29,7 @@ import java.util.jar.Manifest;
  */
 public class VersionUtil {
 
+    private final static Logger LOG = LoggerFactory.getLogger(GalenUtils.class);
 
     public static String getVersion(Class<?> clazz) {
         String implementationVersion = getVersion(clazz, "Implementation-Version");
@@ -65,7 +68,7 @@ public class VersionUtil {
             }
             return version;
         } catch (Exception e) {
-            // log.error(e.getMessage(), e);
+            LOG.debug("Could not read manifest info", e);
         }
         return StringUtils.EMPTY;
     }
--- galen-core/src/test/java/com/galenframework/tests/utils/VersionTest.java
+++ galen-core/src/test/java/com/galenframework/tests/utils/VersionTest.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2015 Ivan Shubin http://galenframework.com
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,6 +32,8 @@ public class VersionTest {
     public void shouldAlwaysReturnStringAndNotNull() throws Exception {
         String customVersion = VersionUtil.getVersion(By.class, "Custom-Version");
         assertThat(customVersion, not(nullValue()));
+        String defaultVersion = VersionUtil.getVersion(By.class);
+        assertThat(defaultVersion, not(nullValue()));
     }
 
     @Test
