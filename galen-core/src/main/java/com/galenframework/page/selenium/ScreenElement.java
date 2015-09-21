86ac75ba3807dcbecedb3cae59a966306c6dd815
Ivan Shubin <ivan.ishubin@gmail.com>
2015-09-21 21:05:21 +0200

    Fixed screen area calculation

--- galen-core/src/main/java/com/galenframework/page/selenium/ScreenElement.java
+++ galen-core/src/main/java/com/galenframework/page/selenium/ScreenElement.java
@@ -36,11 +36,9 @@ public class ScreenElement extends PageElement {
     @Override
     public Rect calculateArea() {
         List<Long> size = (List<Long>)((JavascriptExecutor)driver).executeScript("return [Math.max(" +
-                    "document.body.scrollWidth, document.documentElement.scrollWidth," + 
                     "document.body.offsetWidth, document.documentElement.offsetWidth," +
                     "document.body.clientWidth, document.documentElement.clientWidth)," +
                     "Math.max(" + 
-                    "document.body.scrollHeight, document.documentElement.scrollHeight," +
                     "document.body.offsetHeight, document.documentElement.offsetHeight," +
                     "document.body.clientHeight, document.documentElement.clientHeight)];"
                 );
