c3dbdb7310f3590d5f7869a19e38615498fc3e37
Ivan Shubin <ivan.ishubin@gmail.com>
2015-09-26 11:06:06 +0200

    Removed setting './' in contextPath

--- galen-core/src/main/java/com/galenframework/speclang2/reader/pagespec/PageSpecReader.java
+++ galen-core/src/main/java/com/galenframework/speclang2/reader/pagespec/PageSpecReader.java
@@ -40,9 +40,6 @@ public class PageSpecReader {
                          Map<String, Object> jsVariables, Map<String, Locator> objects) throws IOException {
 
         String contextPath = GalenUtils.getParentForFile(path);
-        if (contextPath == null) {
-            contextPath = "./";
-        }
 
         InputStream stream = GalenUtils.findFileOrResourceAsStream(path);
         if (stream == null) {
