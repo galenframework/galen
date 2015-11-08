72e47ab85103c092384412a69c0503a8355ce19f
Ivan Shubin <ivan.ishubin@gmail.com>
2015-11-08 09:30:01 +0100

    Fixed component frame integration test

--- galen-integration-tests/src/test/java/com/galenframework/tests/integration/ComponentFrameIT.java
+++ galen-integration-tests/src/test/java/com/galenframework/tests/integration/ComponentFrameIT.java
@@ -17,11 +17,10 @@ package com.galenframework.tests.integration;
 
 import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.emptyCollectionOf;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.galenframework.api.Galen;
 import com.galenframework.page.Rect;
@@ -65,15 +64,26 @@ public class ComponentFrameIT {
     public void shouldTest_componentFrameSpec_andReportFailureProperly() throws IOException {
         LayoutReport layoutReport = Galen.checkLayout(driver, findSpec("/frame-page/failed.spec"), asList("desktop"));
 
-        assertThat(layoutReport.getValidationErrorResults(), contains(new ValidationResult()
-                .withObjects(asList(new ValidationObject(new Rect(8, 32, 302, 152), "frame")))
-                .withError(new ValidationError(asList("Child component spec contains 1 errors")))
-                .withChildValidationResults(asList(
-                        new ValidationResult()
-                                .withObjects(asList(new ValidationObject(new Rect(16, 89, 176, 20), "frame-link")))
-                                .withError(new ValidationError(asList("\"frame-link\" height is 20px instead of 40px")))
-                ))
-        ));
+
+        assertThat(layoutReport.getValidationErrorResults().size(), is(1));
+        ValidationResult validationResult = layoutReport.getValidationErrorResults().get(0);
+
+        assertThat(validationResult.getValidationObjects().size(), is(1));
+        ValidationObject validationObject = validationResult.getValidationObjects().get(0);
+
+        assertThat(validationObject.getArea().getLeft(), is(8));
+        assertThat(validationObject.getArea().getTop(), allOf(greaterThan(30), lessThan(35)));
+        assertThat(validationObject.getArea().getWidth(), allOf(greaterThan(300), lessThan(304)));
+        assertThat(validationObject.getArea().getHeight(), allOf(greaterThan(150), lessThan(154)));
+
+        assertThat(validationResult.getError(), is(new ValidationError(asList("Child component spec contains 1 errors"))));
+
+        List<ValidationResult> childResults = validationResult.getChildValidationResults();
+        assertThat(childResults.size(), is(1));
+
+        assertThat("validation error should match regex",
+                childResults.get(0).getError().getMessages().get(0).matches("\"frame-link\" height is (20|21|22|23|24|25)px instead of 40px"),
+                is(true));
     }
 
     private String findSpec(String path) {
