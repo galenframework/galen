38efdbd0f18dbf737a4302f408833c133c4f1c71
Ivan Shubin <ivan.ishubin@gmail.com>
2015-11-27 22:38:26 +0100

    Added a check for unfinished changelog

--- release.sh
+++ release.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-set -e
+set +e
 
+die() {
+    echo -e "\e[31m$1\e[0m"
+    exit 1
+}
+
+verify_version_data() {
+    match=`echo $1 | grep -Ex '##.*\[([0-9]{1,}\.){2}[0-9]{1,}\]\[[0-9]{4}(\-[0-9]{2}){2}\]'`
+    if [[ ! -n "$match" ]]; then
+        die "Incorrect header in changelog: $1"
+    fi
+}
+
+while read -r line ; do
+    verify_version_data "$line"
+done < <(grep "\\#.*\\[" CHANGELOG.md)
+
+
+set -e
 
 mvn release:prepare -DskipTests=true -Darguments='-DskipTests=true'
 
