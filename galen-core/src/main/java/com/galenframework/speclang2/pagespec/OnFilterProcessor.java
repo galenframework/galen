add89ab907fc7b2044b0528152fd5726cabd5951
Ivan Shubin <ivan.ishubin@gmail.com>
2016-03-27 12:24:26 +0200

    Updated license headers

--- galen-core/src/main/java/com/galenframework/speclang2/pagespec/OnFilterProcessor.java
+++ galen-core/src/main/java/com/galenframework/speclang2/pagespec/OnFilterProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2015 Ivan Shubin http://galenframework.com
+* Copyright 2016 Ivan Shubin http://galenframework.com
 * 
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
