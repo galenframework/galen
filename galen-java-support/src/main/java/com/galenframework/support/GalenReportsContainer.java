246845aa993c6a09b64ceeaf6a6f26889144626e
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-11-14 15:09:30 +0000

    fix threading issues, see #451

--- galen-java-support/src/main/java/com/galenframework/support/GalenReportsContainer.java
+++ galen-java-support/src/main/java/com/galenframework/support/GalenReportsContainer.java
@@ -1,23 +1,22 @@
 /*******************************************************************************
-* Copyright 2016 Ivan Shubin http://galenframework.com
-* 
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* 
-*   http://www.apache.org/licenses/LICENSE-2.0
-* 
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-******************************************************************************/
+ * Copyright 2016 Ivan Shubin http://galenframework.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package com.galenframework.support;
 
 import com.galenframework.reports.GalenTestInfo;
 import com.galenframework.reports.TestReport;
-import org.testng.annotations.TestInstance;
 
 import java.lang.reflect.Method;
 import java.util.LinkedList;
@@ -33,30 +32,31 @@ public enum GalenReportsContainer {
 
     /**
      * Returns a single instance of {@link GalenReportsContainer}
+     *
      * @return an instance of {@link GalenReportsContainer}
      */
-    public static GalenReportsContainer get() {
+    public static synchronized GalenReportsContainer get() {
         return INSTANCE;
     }
 
-    public TestReport registerTest(Method method) {
+    public synchronized TestReport registerTest(Method method) {
         GalenTestInfo testInfo = GalenTestInfo.fromMethod(method);
         tests.add(testInfo);
         return testInfo.getReport();
     }
 
-    public TestReport registerTest(String name, List<String> groups) {
+    public synchronized TestReport registerTest(String name, List<String> groups) {
         GalenTestInfo testInfo = GalenTestInfo.fromString(name, groups);
         tests.add(testInfo);
         return testInfo.getReport();
     }
 
-    public TestReport registerTest(GalenTestInfo testInfo) {
+    public synchronized TestReport registerTest(GalenTestInfo testInfo) {
         tests.add(testInfo);
         return testInfo.getReport();
     }
 
-    public List<GalenTestInfo> getAllTests() {
+    public synchronized List<GalenTestInfo> getAllTests() {
         return tests;
     }
 }
