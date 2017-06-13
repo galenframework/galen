5a9fba91dd566ca061a9ad106b88db1f5f621710
Reinhardt, Martin <contact@martinreinhardt-online.de>
2017-06-13 08:30:53 +0200

    Adding docker based integration tests for #494 and https://github.com/hypery2k/galenframework-cli/issues/43

--- integration-test.sh
+++ integration-test.sh
@@ -1,1 +1,9 @@
+docker run -d -p 4444:4444 -e SE_OPTS="-debug true" --name selenium-hub selenium/hub:3.2.0-actinium
+docker run -d --link selenium-hub:hub --name node-chrome selenium/node-chrome:3.2.0-actinium
+docker run -d --link selenium-hub:hub --name node-firefox selenium/node-firefox:3.2.0-actinium
+docker run -d --link selenium-hub:hub --name node-phantomjs selenium/node-phantomjs:3.2.0-actinium
 mvn verify
+docker stop selenium-hub
+docker stop node-chrome
+docker stop node-firefox
+docker stop node-phantomjs
